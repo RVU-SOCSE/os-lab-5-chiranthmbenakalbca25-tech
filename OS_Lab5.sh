9d41179332cac79a566d26487761adcb2f41de91
Chiranth M Benakal BCA <chiranthmbenakalbca25@rvu.edu.in>
2026-03-30 14:57:22 +0530

    Add files via upload

---
+++ OS_Lab5.sh
@@ -0,0 +1,26 @@
+echo "File permission & ownership"
+
+#Get filename
+echo "Enter file name"
+read file
+
+#Check file exists
+if [ ! -f "$file" ]; then
+   echo "File not found!"
+   exit
+fi
+
+#Get permission
+echo "Enter permission (e.g., 755):"
+read perm
+
+#change owner
+echo "Enter new owner name:"
+read owner
+
+#Change ownership
+chown $owner $file
+
+#Display new permissions
+echo "Updated file details:"
+ls -l $file
